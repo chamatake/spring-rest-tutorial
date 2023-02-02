61db1836a460acf9bfcb7ca135486cd45be52129
Chris Hamatake <chamatake@gmail.com>
2023-02-02 11:15:38 -0700

    finish order controller endpoints and update database initializer

--- src/main/java/bwg/tutorials/resttutorial/LoadDatabase.java
+++ src/main/java/bwg/tutorials/resttutorial/LoadDatabase.java
@@ -1,6 +1,7 @@
 package bwg.tutorials.resttutorial;
 
 import bwg.tutorials.resttutorial.repository.EmployeeRepository;
+import bwg.tutorials.resttutorial.repository.OrderRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -12,10 +13,16 @@ public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
-            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "ring bearer")));
+            employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+            employeeRepository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+            orderRepository.save(new Order("brownies", Status.COMPLETED));
+            orderRepository.save(new Order("ice cream", Status.IN_PROGRESS));
+            orderRepository.save(new Order("nap time", Status.CANCELED));
+            orderRepository.findAll().forEach(order -> log.info("Preloaded " + order));
         };
     }
 }
--- src/main/java/bwg/tutorials/resttutorial/controller/OrderController.java
+++ src/main/java/bwg/tutorials/resttutorial/controller/OrderController.java
@@ -78,6 +78,19 @@ public class OrderController {
 
     @PutMapping("/orders/{id}/complete")
     public ResponseEntity<?> complete(@PathVariable Long id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
 
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(Problem.create()
+                        .withTitle("Method not allowed")
+                        .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
     }
 }
